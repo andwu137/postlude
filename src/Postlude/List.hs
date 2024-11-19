185af9afe08c1e4e3e9801d848ca976c65bb7b82
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-11-19 11:38:15 -0500

    feat(List): add more helpers

--- src/Postlude/List.hs
+++ src/Postlude/List.hs
@@ -9,6 +9,14 @@ module Postlude.List (
 
     -- ** Searching With Predicate
     partition,
+
+    -- * Sublists
+
+    -- ** Extracting Sublists
+    halve,
+
+    -- * Joining
+    mergeRuns,
 ) where
 
 import Postlude.Base
@@ -29,6 +37,25 @@ instance Semigroup (List a) where
 {- Functions -}
 singleton :: a -> List a
 singleton x = Cons x Nil
+
+mergeRuns :: (Ord a) => List a -> List a -> List a
+mergeRuns = \cases
+    xs Nil -> xs
+    Nil ys -> ys
+    (Cons x xs) (Cons y ys)
+        | x < y -> Cons x (mergeRuns xs (Cons y ys))
+        | otherwise -> Cons y (mergeRuns (Cons x xs) ys)
+
+halve :: List a -> (List a, List a)
+halve list =
+    go list list
+  where
+    go = \cases
+        ss Nil -> (Nil, ss)
+        ss (Cons _ Nil) -> (Nil, ss)
+        (Cons s ss) (Cons _ (Cons _ fs)) ->
+            (\(~(h, t)) -> (Cons s h, t)) (go ss fs)
+        Nil _ -> (Nil, Nil)
 
 partition :: (a -> Bool) -> List a -> (List a, List a)
 partition p =
