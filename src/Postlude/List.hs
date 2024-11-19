11c07e1e4d35fa34dfd5c04b5df79ad38d6148aa
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-11-19 11:12:02 -0500

    feat(List): add helpers refactor(ListF): move ListF to subdirectory of List

--- src/Postlude/List.hs
+++ src/Postlude/List.hs
@@ -1,22 +1,41 @@
 module Postlude.List (
+    -- * Data Types
     List (..),
-    ListF (..),
+
+    -- * Basic Functions
+    singleton,
+
+    -- * Searching
+
+    -- ** Searching With Predicate
+    partition,
 ) where
 
 import Postlude.Base
-import Postlude.Functor
+import Postlude.Bool (otherwise)
+import Postlude.Semigroup
 
+{- List -}
 data List a
     = Nil
     | Cons a (List a)
     deriving (Show, Eq, Ord)
 
-data ListF a f
-    = FNil
-    | FCons a f
-    deriving (Show, Eq, Ord)
+instance Semigroup (List a) where
+    Nil <> ys = ys
+    xs <> Nil = xs
+    (Cons x xs) <> ys = Cons x (xs <> ys)
 
-instance Functor (ListF a) where
-    map f = \case
-        FNil -> FNil
-        FCons x xs -> FCons x (f xs)
+{- Functions -}
+singleton :: a -> List a
+singleton x = Cons x Nil
+
+partition :: (a -> Bool) -> List a -> (List a, List a)
+partition p =
+    go
+  where
+    go = \case
+        Nil -> (Nil, Nil)
+        Cons x xs
+            | p x -> (\(~(f, s)) -> (Cons x f, s)) (go xs)
+            | otherwise -> (\(~(f, s)) -> (f, Cons x s)) (go xs)
