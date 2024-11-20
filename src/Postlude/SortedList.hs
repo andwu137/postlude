57e50ff84b5566b56b66fd2fe69b16ac274901be
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-11-19 21:20:40 -0500

    feat(SortedList): add naive always sorted list data type

---
+++ src/Postlude/SortedList.hs
@@ -0,0 +1,37 @@
+module Postlude.SortedList (
+    SortedList (..),
+    fromList,
+) where
+
+import Postlude.Base
+import Postlude.Empty
+import Postlude.Functor
+import Postlude.Index
+import Postlude.List as List ()
+import qualified Postlude.List as List
+import qualified Postlude.List.Immutable.Sort as List.Sort
+import Postlude.Pure
+import Postlude.Semigroup
+import Postlude.Unit
+
+newtype SortedList a
+    = UnsafeSortedList [a]
+    deriving
+        ( Show
+        , Eq
+        , Ord
+        , Functor
+        , Pure
+        , Empty
+        , Unit
+        )
+
+instance (Ord a) => Semigroup (SortedList a) where
+    (UnsafeSortedList xs) <> (UnsafeSortedList ys) =
+        UnsafeSortedList (List.mergeRuns xs ys)
+
+instance Index SortedList where
+    index n (UnsafeSortedList xs) = index n xs
+
+fromList :: (Ord a) => [a] -> SortedList a
+fromList xs = UnsafeSortedList (List.Sort.mergeSort xs)
