6401a9dc2ef915cd19cbb9fa840d3a3276915cf5
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-11-19 22:35:46 -0500

    feat(ListZipper): Initialize data type

---
+++ src/Postlude/ListZipper.hs
@@ -0,0 +1,38 @@
+module Postlude.ListZipper (
+    ListZipper (..),
+) where
+
+import Postlude.Base
+import Postlude.Foldable
+import Postlude.Functor
+import Postlude.List ()
+import Postlude.Pure
+
+data ListZipper a
+    = UnsafeListZipper
+    { _leftList :: [a]
+    , current :: a
+    , _rightList :: [a]
+    }
+    deriving
+        ( Show
+        , Eq
+        , Ord
+        )
+
+instance Functor ListZipper where
+    map f (UnsafeListZipper{..}) =
+        UnsafeListZipper
+            { _leftList = f <$> _leftList
+            , current = f current
+            , _rightList = f <$> _rightList
+            }
+
+instance Pure ListZipper where
+    pure x = UnsafeListZipper [] x []
+
+instance Foldable ListZipper where
+    foldr f d (UnsafeListZipper{..}) =
+        let rs = foldr f d _rightList
+            cs = f current rs
+         in foldr f cs _leftList
